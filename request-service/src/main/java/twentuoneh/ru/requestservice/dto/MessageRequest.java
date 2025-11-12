591ed7cb766fb7ec254a523b3ff4ce8acbdc70e9
Dmitry Kalinin <didiwot@yandex.ru>
2025-11-12 15:34:10 +0300

    Изменил DTO

--- request-service/src/main/java/twentuoneh/ru/requestservice/dto/MessageRequest.java
+++ request-service/src/main/java/twentuoneh/ru/requestservice/dto/MessageRequest.java
@@ -1,6 +1,7 @@
 package twentuoneh.ru.requestservice.dto;
 
 import lombok.Data;
+import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
 import twentuoneh.ru.requestservice.enums.Assistant;
 
 @Data
@@ -8,4 +9,8 @@ public class MessageRequest {
     String sessionId;
     String message;
     Assistant assistant;
+
+    Long userId;
+    String userEmail;
+    String userName;
 }
--- request-service/src/main/java/twentuoneh/ru/requestservice/service/assistants/DefaultAssistantService.java
+++ request-service/src/main/java/twentuoneh/ru/requestservice/service/assistants/DefaultAssistantService.java
@@ -16,9 +16,26 @@ public class DefaultAssistantService implements AssistantService {
     @Override
     public MessageResponse handle(MessageRequest request) {
         var assistant = request.getAssistant().assistantName();
+
+//        User user = new User().builder().
+
+
+//        Session session = new Session().builder().
+//        userId(request.getUserId()).
+//        assistantRole(request.getAssistant().assistantName()).
+//                build();
+
+
+//
+//        Message message = new Message();
+//        message.setSessionId(sessionId);
+//        message.setRole("user"); // для пользовательских сообщений
+//        message.setText(request.getMessage());
+
 //        var chatMessage = new ChatMessage(Assistant.CONSULTANT.assistantName(), );
         //TODO: Переписать
         //String result = llm.generate(assistant, , request.getMessage());
-        return new MessageResponse(result);
+//        return new MessageResponse(result);
+        return null;
     }
 }
